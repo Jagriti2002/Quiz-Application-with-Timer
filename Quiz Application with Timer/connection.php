14ec7ff8907c2c418fbb4d414eeb58c3392bbd5f
Jagriti Mishra <103900450+Jagriti2002@users.noreply.github.com>
2023-01-17 19:27:04 +0530

    Add files via upload

---
+++ Quiz Application with Timer/connection.php
@@ -0,0 +1,9 @@
+<?php
+    $dbhost= "localhost";
+    $dbuser = "";
+    $dbpass = "";
+    $dbname = "quizdata";
+
+    $connection = mysqli_connect($dbhost,$dbuser,$dbpass,$dbname);
+
+?>
---
+++ Quiz Application with Timer/js/questions.js
@@ -0,0 +1,57 @@
+let questions = [
+    {
+    numb: 1,
+    question: "What does Code_Clause provides?",
+    answer: "Internship",
+    options: [
+      "Internship",
+      "Festive",
+      "Dance",
+      "Collection"
+    ]
+  },
+    {
+    numb: 2,
+    question: "What skills does Code_Clause provides?",
+    answer: "Software Coding",
+    options: [
+      "Dancing",
+      "Playing",
+      "Software Coding",
+      "Business"
+    ]
+  },
+    {
+    numb: 3,
+    question: "Where does Code_Clause Office locates?",
+    answer: "Pune",
+    options: [
+      "Mumbai",
+      "Delhi",
+      "Pune",
+      "Chennai"
+    ]
+  },
+    {
+    numb: 4,
+    question: "What does SQL stand for?",
+    answer: "Structured Query Language",
+    options: [
+      "Stylish Question Language",
+      "Stylesheet Query Language",
+      "Statement Question Language",
+      "Structured Query Language"
+    ]
+  },
+    {
+    numb: 5,
+    question: "What does Code_Clause dont provide?",
+    answer: "Travelling",
+    options: [
+      "Web Development",
+      "Data Science",
+      "Travelling",
+      "Content Writer"
+    ]
+  },
+];
